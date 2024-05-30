058667c91951be4963f0f3cca05411a4c0143da5
abdelwhab Eladawy <abdelwahabeladawy@gmail.com>
2024-05-30 16:20:24 +0300

    make a data in local storage

--- js/index.js
+++ js/index.js
@@ -2,25 +2,24 @@ var siteName = document.getElementById("name");
 var siteURL = document.getElementById("siteURL");
 
 var sitesInfo = [];
-if (sitesInfo!==null){
-  sitesInfo=JSON.parse(localStorage.getItem("list"))
-  display(sitesInfo)
+if (sitesInfo !== null) {
+  sitesInfo = JSON.parse(localStorage.getItem("list"));
+  display(sitesInfo);
 }
 
 function getInfo() {
-  
-  if(nameValidation()){
-  var site = {
-    name: siteName.value,
-    link: siteURL.value,
-  };
-  sitesInfo.push(site);
-  display(sitesInfo);
-  localStorage.setItem("list",JSON.stringify(sitesInfo))
-  clearInputs()
-  siteName.classList.remove("is-valid")
-  siteURL.classList.remove("is-valid")
-}
+  if (nameValidation()) {
+    var site = {
+      name: siteName.value,
+      link: siteURL.value,
+    };
+    sitesInfo.push(site);
+    display(sitesInfo);
+    localStorage.setItem("list", JSON.stringify(sitesInfo));
+    clearInputs();
+    siteName.classList.remove("is-valid");
+    siteURL.classList.remove("is-valid");
+  }
 }
 
 function display(list) {
@@ -37,55 +36,46 @@ function display(list) {
   }
   document.getElementById("display-Data").innerHTML = container;
 }
-function clearInputs(){
-  siteName.value=""
-  siteURL.value=""
+function clearInputs() {
+  siteName.value = "";
+  siteURL.value = "";
 }
-
 
 function deleteItem(i) {
-  sitesInfo.splice(i,1)
-  display(sitesInfo)
-  localStorage.setItem("list",JSON.stringify(sitesInfo))
+  sitesInfo.splice(i, 1);
+  display(sitesInfo);
+  localStorage.setItem("list", JSON.stringify(sitesInfo));
 }
 
-function nameValidation(){
-  var regex=/^[A-Z]([a-z]|[0-9])+/
-  var nameError=document.getElementById("nameError")
-  if(regex.test(siteName.value)===true){
-    siteName.classList.add("is-valid")
-    siteName.classList.remove("is-invalid")
-    nameError.classList.add("d-none")
-    return true
-
-  }else {
-    siteName.classList.add("is-invalid")
-    siteName.classList.remove("is-valid")
-    nameError.classList.remove("d-none")
-    return false
+function nameValidation() {
+  var regex = /^[A-Z]([a-z]|[0-9])+/;
+  var nameError = document.getElementById("nameError");
+  if (regex.test(siteName.value) === true) {
+    siteName.classList.add("is-valid");
+    siteName.classList.remove("is-invalid");
+    nameError.classList.add("d-none");
+    return true;
+  } else {
+    siteName.classList.add("is-invalid");
+    siteName.classList.remove("is-valid");
+    nameError.classList.remove("d-none");
+    return false;
   }
-
 }
 
-
-
-function validateUrl(){
-var regex=/^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/
-urlError=document.getElementById("urlError")
-if(regex.test(siteURL.value)===true){
-  siteURL.classList.add("is-valid")
-  siteURL.classList.remove("is-invalid")
-  urlError.classList.add("d.none")
-  return true
+function validateUrl() {
+  var regex =
+    /^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
+  urlError = document.getElementById("urlError");
+  if (regex.test(siteURL.value) === true) {
+    siteURL.classList.add("is-valid");
+    siteURL.classList.remove("is-invalid");
+    urlError.classList.add("d.none");
+    return true;
+  } else {
+    siteURL.classList.remove("is-valid");
+    siteURL.classList.add("is-invalid");
+    urlError.classList.remove("d.none");
+    return false;
+  }
 }
-else{
-  siteURL.classList.remove("is-valid")
-  siteURL.classList.add("is-invalid")
-  urlError.classList.remove("d.none")
-  return false
-}
-}
-
-
-
-
